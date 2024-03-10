685f6edcf49b705f2e6d1c90459b418b97fd588d
Alexander Garustovich <yukigaru@gmail.com>
2024-03-10 21:13:21 +0300

    Add unit-tests for exercise 6

--- src/task-6/main.cpp
+++ src/task-6/main.cpp
@@ -2,58 +2,155 @@
 #include <deque>
 #include <mutex>
 #include <condition_variable>
+#include <algorithm>
+#include <atomic>
 #include <thread>
 #include <chrono>
 #include <vector>
+#include "tests.h"
 
 
 template <typename T>
 class ConcurrentFIFOQueue {
 public:
-    // note: добавлен лимит на размер очереди
-    ConcurrentFIFOQueue(std::uint64_t limit): _limit(limit) {
+    // добавлен лимит на размер очереди
+    ConcurrentFIFOQueue(size_t limit = 0): _limit(limit) {
     }
 
     void push(const T &val) {
-        // ждёт, если очередь переполнена
     }
 
     T pop() {
-        // ждёт, если нет элементов
         return T{};
     }
 
 private:
     std::mutex _m;
     std::condition_variable _cv;
+
     std::deque<T> _queue;
-    std::uint64_t _limit;
+    size_t _limit;
 };
 
 
-void worker_thread_func(ConcurrentFIFOQueue<int> &queue, unsigned idx) {
-    while (true) {
-        auto v = queue.pop();
-        std::cout << "thread " << idx << ": got value " << v << std::endl;
+/*
+ * Tests
+ */
+void test_multiple_push_pop() {
+    ConcurrentFIFOQueue<int> queue;
+
+    queue.push(1);
+    queue.push(2);
+    queue.push(3);
+
+    EXPECT(queue.pop() == 1);
+    EXPECT(queue.pop() == 2);
+    EXPECT(queue.pop() == 3);
+
+    PASS();
+}
+
+void test_pop_wait() {
+    ConcurrentFIFOQueue<int> queue;
+    std::atomic<bool> item_popped{false};
+
+    std::thread consumer{[&]() {
+        queue.pop();
+        item_popped.store(true);
+    }};
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    EXPECT(item_popped.load() == false);
+
+    queue.push(1);
+
+    consumer.join();
+
+    EXPECT(item_popped.load() == true);
+
+    PASS();
+}
+
+void test_push_wait() {
+    constexpr auto Limit = 2u;
+    ConcurrentFIFOQueue<int> queue{Limit};
+
+    std::atomic_int values_pushed{0};
+
+    std::thread producer([&]() {
+        for (int i = 0; i < Limit + 1; ++i) {
+            queue.push(i);
+            values_pushed++;
+        }
+    });
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    EXPECT(values_pushed.load() == Limit);
+
+    queue.pop();
+    producer.join();
+
+    EXPECT(values_pushed.load() == Limit+1);
+
+    PASS();
+}
+
+void test_multiple_threads() {
+    constexpr int NumThreads = 4;
+    constexpr auto N = 100; // каждый producer поток производит N чисел
+
+    ConcurrentFIFOQueue<int> queue{2}; // лимит в 2 элемента
+
+    std::vector<int> consumed;
+    std::mutex consumed_mutex;
+
+    auto producer_func = [&](int thread_id) {
+        for (int i = 0; i < N; ++i) {
+            int num = thread_id * N + i;
+            queue.push(num);
+        }
+    };
+
+    auto consumer_func = [&]() {
+        for (int i = 0; i < N; ++i) {
+            int num = queue.pop();
+
+            std::lock_guard<std::mutex> lock(consumed_mutex);
+            consumed.push_back(num);
+        }
+    };
+
+    std::vector<std::thread> threads;
+
+    for (int i = 0; i < NumThreads; ++i) {
+        threads.emplace_back(producer_func, i);
+        threads.emplace_back(consumer_func);
     }
+
+    for (auto& t : threads) {
+        t.join();
+    }
+
+    EXPECT(consumed.size() == N * NumThreads);
+
+    std::sort(std::begin(consumed), std::end(consumed));
+
+    for (int i = 1; i < N; ++i) {
+        EXPECT(consumed[i] == consumed[i-1] + 1);
+    }
+
+    PASS();
 }
 
 int main() {
-    // очередь на 4 элемента
-    ConcurrentFIFOQueue<int> q{4};
+    try {
+        test_multiple_push_pop();
+        test_pop_wait();
+        test_push_wait();
+        test_multiple_threads();
 
-    std::vector<std::thread> threads;
-    for (auto i = 0u; i < 8; i++) {
-        threads.emplace_back(std::thread{worker_thread_func, std::ref(q), i});
-    }
-
-    // наполняем значениями
-    for (auto i = 0u; i < 256; i++) {
-        q.push(rand());
-    }
-
-    for (auto &t : threads) {
-        t.join();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
     }
     return 0;
 }
