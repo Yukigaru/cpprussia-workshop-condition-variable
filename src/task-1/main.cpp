f9c1dee83a92486f633bcd9bec79e962b54c0b5a
Alexander Garustovich <yukigaru@gmail.com>
2024-03-15 22:00:16 +0300

    Minor update to exercise 1

--- src/task-1/main.cpp
+++ src/task-1/main.cpp
@@ -10,7 +10,7 @@
 Необходимо переписать с использованием condition_variable.
 */
 
-std::atomic_bool resume;
+std::atomic_bool resume{false};
 
 void another_thread_func() {
     std::cout << "another thread: waiting..." << std::endl;
@@ -23,13 +23,13 @@ void another_thread_func() {
 }
 
 int main() {
-    std::thread t(another_thread_func);
+    std::thread t{another_thread_func};
 
     std::this_thread::sleep_for(std::chrono::seconds(3));
     std::cout << "main: signaling the other thread to resume" << std::endl;
 
-    // TODO: заменить на condition_variable
     resume.store(true);
+    // TODO: notify a condition_variable
 
     t.join();
     return 0;
